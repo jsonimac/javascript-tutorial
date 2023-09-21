9a382bba1604b9c93cd0b88e71c319f2f1fb5ec2
Jessie S. Caminos <61927149+jsonimac@users.noreply.github.com>
2023-09-21 18:38:39 +0800

    add lessons

--- 01-Fundamentals-Javascript/starter/intro.js
+++ 01-Fundamentals-Javascript/starter/intro.js
@@ -1,18 +1,59 @@
 // Discuss about naming convention.
 // Be Detailed as possible.
-let firstName = 'Jessie';
-var lastName = 'Caminos';
+let firstName = "Jessie";
+var lastName = "Caminos";
 const place = "Ozamis City";
 
-console.log(firstName + ' ' + secondName + ' ' + place);
-
+console.log(firstName + " " + lastName + " " + place);
 
 // Data Types
-let firstNumber = 10; //Floating point number which it awlays have decimal point.
-let firstCar = "Ford Ranger"; //Inside double quotes is a string.
-let believer = true; //Boolean value which is either true or false.
-let dog = undefined; //Undefined value which is not yet defined.
+// let firstNumber = 10; //Floating point number which it awlays have decimal point.
+// let firstCar = "Ford Ranger"; //Inside double quotes is a string.
+// let believer = true; //Boolean value which is either true or false.
+// let dog; //Undefined value which is not yet defined.
+// console.log(typeof firstNumber)
+// console.log(typeof firstCar)
+// console.log(typeof believer)
+// console.log(typeof dog)
 
+// Using let can make you reassign the value of the variable.
+// let skill = "coding";
+// skill = "driving";
+// console.log(skill);
 
+// Using const can't make you reassign the value of the variable.
+// const personName = "Jessie";
+// personName = "Caminos"; // This will throw an error.
 
- 
+// Use const AS DEFAULT in declaring variables.
+
+// -------------------------------------------------
+// Operators
+// STUDY OPERATOR PRECEDENCE
+// const now = 2023;
+// const jessieAge = now - 1999;
+// const markAge = now - 1988;
+// let count = 10;
+// console.log(jessieAge, markAge);
+// console.log(jessieAge * 2);
+// console.log(jessieAge / 10);
+// console.log(count ** 3);
+// count++;
+// count--;
+// count += 10; // count = count + 10
+// console.log(now - 1999 > now - 1978, (count += 10));
+// -------------------------------------------------
+
+// -------------------------------------------------
+// Challenge 1
+// const massMark = 78,
+//   massJohn = 92;
+// const heightMark = 1.69,
+//   heightJohn = 1.95;
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark);
+// console.log(BMIJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
+// console.log(markHigherBMI);
+// -------------------------------------------------
