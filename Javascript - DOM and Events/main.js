d691490fbe5c1a4be290fdf890c1b4e3c3f86ae7
Jessie S. Caminos <61927149+jsonimac@users.noreply.github.com>
2023-11-24 20:07:39 +0800

    learn about Dom and Manipulating html with js
    Learn about Dom
    Dom is the representation of both html and javascript that interacts each other. On how js access those html data

---
+++ Javascript - DOM and Events/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.querySelector('.score').textContent = 21;
+document.querySelector('.highscore').textContent = 30;
+document.querySelector('.message').textContent = 'Successfully Guest!';
+document.querySelector('.number').textContent = 45
+
+// log the value of the field
+let val = document.querySelector('.guess').value = 21;
+console.log(val);
