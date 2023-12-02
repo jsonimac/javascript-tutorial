6237ff5ff1c59342b43cd4ed165ce7015d3dda63
Jessie S. Caminos <61927149+jsonimac@users.noreply.github.com>
2023-12-02 23:01:30 +0800

    Update main.js

--- Javascript - DOM and Events/Project 2 - Modal Window/main.js
+++ Javascript - DOM and Events/Project 2 - Modal Window/main.js
@@ -9,28 +9,42 @@ const overlay = document.querySelector('.overlay')
 const buttonClose = document.querySelector('.close-modal')
 const buttonOpen = getDom('.show-modal')
 
-// Loop in for loop
+// --- Loop in For Of ---
+for (const i of buttonOpen) {
+  i.addEventListener('click', handlOpen)
+}
+
+// ------------------------------------------------------------
+// --- Loop in For LOOP ---
 // for (let i = 0; i < buttonOpen.length; i++) {
 //   buttonOpen[i].addEventListener('click', handlOpen)
 // }
+// ------------------------------------------------------------
+
+
 function handlOpen() {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 }
-// close modal
-buttonClose.addEventListener('click', handleClose);
-overlay.addEventListener('click', handleClose);
 
 function handleClose(){
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 }
 
-document.addEventListener('keypress', function(){
-  console.log(`you press somting`);
+// close modal
+// buttonClose.addEventListener('click', handleClose);
+// overlay.addEventListener('click', handleClose);
+
+
+document.addEventListener('keydown', function(e){
+  e.key.match('Escape') ? handleClose : ''
 });
 
-// Loop in for of
-for (const i of buttonOpen) {
-  i.addEventListener('click', handlOpen)
-}
+
+
+
+// function handleKeypress(e) {
+//   let keyValue = e.key == "Escape" ? 'You enter escape' : 'Wrong Key'
+//   console.log(keyValue);
+// }
