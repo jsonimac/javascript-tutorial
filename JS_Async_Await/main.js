7fcc27c8700ea01d9a0889c91bd365f1e52c5281
Jessie S. Caminos <61927149+jsonimac@users.noreply.github.com>
2023-12-16 01:26:10 +0800

    fetch.then

--- JS_Async_Await/main.js
+++ JS_Async_Await/main.js
@@ -68,64 +68,64 @@
 
 
 
-const renderData = function(data, table){
-  // const langu = Object.values(data.languages);
-  const html = `
-    <tr>
-      <td>${data.name.common}</td>
-      <td>${data.capital}</td>
-      <td>${data.capital}</td>
-      <td>${data.population}</td>
-    </tr>
-  `;
-  document.querySelector(table).insertAdjacentHTML('beforeend', html);
-}
+// const renderData = function(data, table){
+//   // const langu = Object.values(data.languages);
+//   const html = `
+//     <tr>
+//       <td>${data.name.common}</td>
+//       <td>${data.capital}</td>
+//       <td>${data.capital}</td>
+//       <td>${data.population}</td>
+//     </tr>
+//   `;
+//   document.querySelector(table).insertAdjacentHTML('beforeend', html);
+// }
 
-const renderEmpty = function(table){
-  const html = `
-    <tr>
-      <td>empty</td>
-      <td>empty</td>
-      <td>empty</td>
-      <td>empty</td>
-    </tr>
-  `;
-  document.querySelector(table).insertAdjacentHTML('beforeend', html);
-}
+// const renderEmpty = function(table){
+//   const html = `
+//     <tr>
+//       <td>empty</td>
+//       <td>empty</td>
+//       <td>empty</td>
+//       <td>empty</td>
+//     </tr>
+//   `;
+//   document.querySelector(table).insertAdjacentHTML('beforeend', html);
+// }
 
-const getJson = function(url){
-  return fetch(`${url}`)
-    .then(response => {
-      if(!response.ok){
-        throw new Error('country not found')
-      }
-      return response.json()
-    })
-}
+// const getJson = function(url){
+//   return fetch(`${url}`)
+//     .then(response => {
+//       if(!response.ok){
+//         throw new Error('country not found')
+//       }
+//       return response.json()
+//     })
+// }
 
-const getdata = function(name){
-    getJson(`https://restcountries.com/v3.1/name/${name}`)
-    .then(data => {
-      // console.log(data[0].languages);
-      console.log(data);
-      renderData(data[0], '.country')
+// const getdata = function(name){
+//     getJson(`https://restcountries.com/v3.1/name/${name}`)
+//     .then(data => {
+//       // console.log(data[0].languages);
+//       console.log(data);
+//       renderData(data[0], '.country')
 
-      const neigbr = data[0].borders?.[0];
-      if(!neigbr) return new Error('No neighbor found');
+//       const neigbr = data[0].borders?.[0];
+//       if(!neigbr) return new Error('No neighbor found');
 
-      return getJson(`https://restcountries.com/v3.1/alpha/${neigbr}`);
-    })
-    .then(data => {
-      console.log(data);
-      renderData(data[0], '.neig');
-    })
-    .catch(err => console.log(err))
-}
+//       return getJson(`https://restcountries.com/v3.1/alpha/${neigbr}`);
+//     })
+//     .then(data => {
+//       console.log(data);
+//       renderData(data[0], '.neig');
+//     })
+//     .catch(err => console.log(err))
+// }
 
-getdata('china');
-getdata('usa');
-getdata('philippines');
+// getdata('china');
+// getdata('usa');
+// getdata('philippines');
 
-document.querySelector('.btn-country').addEventListener('click', function(){
-  getdata('canada');
-})
+// document.querySelector('.btn-country').addEventListener('click', function(){
+//   getdata('canada');
+// })
