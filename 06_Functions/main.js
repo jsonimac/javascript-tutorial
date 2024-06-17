93342b5743f216f5026e893497b1f94fca2d7e15
Keep Cod'n <sonimac122699@gmail.com>
2024-06-17 14:50:25 +0800

    Start learning function

--- 06_Functions/main.js
+++ 06_Functions/main.js
@@ -1,30 +1,3 @@
 'use strict';
 
-
-
-
-// First Class Function
-// -----------------------------------
-// -> Treated as values
-// -> Another type of Object
-// const firstFunc = function(data){
-//     return `This is the data: ${data} passed to the function`
-// }
-// console.log(firstFunc('Jessie'));
-// -----------------------------------
-
-
-// Higher Order Function
-// -----------------------------------
-// --> Accepts callback function
-// const sampFunc = function(data){
-//     let [firstLett, ...rest] = data.split(' ');
-//     return firstLett.toLowerCase()
-// }
-// This is the higher order function
-// const higFunc = function(str, fn){
-//     let details = fn(str);
-//     return details;
-// }
-// console.log(higFunc('JESIE CAMINOS dD dd', sampFunc));
-// -----------------------------------
+// Start Learning Functions
