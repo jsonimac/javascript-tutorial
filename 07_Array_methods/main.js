caa83f58e95a3a90dbb017af2478a4eb829ad8cb
Keep Cod'n <sonimac122699@gmail.com>
2024-08-05 14:57:06 +0800

    Added arrays new challenge

--- 07_Array_methods/main.js
+++ 07_Array_methods/main.js
@@ -1,5 +1,108 @@
 "use strict";
 
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+
+
+
+// --- Coding Challege ---
+// const julia = [3,5,2,12,7];
+// const kate = [4,1,15,8,3];
+
+// const finalData = [...dogsKate, ...dogsJulia.slice(1,3)]
+
+// const checkDogs = function(dogsJulia, dogsKate){
+//   // Step 1
+//   const finalJulia = dogsJulia.slice(1,3);
+
+//   //Step 2
+//   const finalData = [...dogsKate, ...finalJulia]
+// s
+//   //Step 3
+//   finalData.forEach(function(year, index){
+//     if(year >= 3){
+//       console.log(`the dog number ${index+1} is Adult, and is ${year} year old`);
+//     }
+//     if(year < 3){
+//       console.log(`the dog number ${index+1} still a puppy, ${year} year old`);
+//     }
+//   })
+// }
+
+// checkDogs(julia, kate)
 // Loop Over arrays
 // const accountMovement = [
 //   100, 300, 5000, -3000, 1000, -900, -800, 980,
